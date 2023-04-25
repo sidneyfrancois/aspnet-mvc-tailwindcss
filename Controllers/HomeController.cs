8a3c47a89c059caa6f97ff72f3410fec2859bdce
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-25 19:17:08 -0300

    add project with tailwind by option:

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult RsvpForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult RsvpForm(GuestResponse guest)
+    {
+        if (ModelState.IsValid)
+        {
+            Repository.AddResponse(guest);
+            return View("Thanks", guest);
+        }
+        else
+        {
+            return View();
+        }
+
+    }
+
+    public ViewResult ListResponses()
+    {
+        return View(Repository.Responses.Where(r => r.WillAttend == true));
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models;
+
+public class GuestResponse
+{
+   [Required(ErrorMessage = "Please, enter your name")]
+   public string? Name { get; set; }
+   public string? Email { get; set; }
+   public string? Phone { get; set; }
+   public bool? WillAttend { get; set; }
+}
